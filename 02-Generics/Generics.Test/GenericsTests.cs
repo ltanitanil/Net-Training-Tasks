3943cb9e9770d84e1b9d63ae37bf147876d7e925
tanu2 <tanu2@DESKTOP-I8E9H25>
2018-12-28 14:06:51 +0300

    added solution for task-2

---
+++ 02-Generics/Generics.Test/GenericsTests.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task.Generics.Test {
+
+    [TestClass]
+    public class GenericsTest {
+
+        #region ListConverter.ConvertToString tests
+        public void ListConverter_ConvertToString_Test<T>(IEnumerable<T> list, string expected, string assertMessage) {
+            var actual = list.ConvertToString();
+            Assert.AreEqual(expected, actual, assertMessage);
+        }
+
+        [TestMethod]
+        [TestCategory("ListConverter.ConvertToString")]
+        public void ListConverter_ConvertToString_Should_Convert_List_To_String() {
+            ListConverter_ConvertToString_Test(
+                   new[] {1,2,3,4,5},
+                   "1,2,3,4,5",
+                   "ListConverter.ConvertToString should convert int list to string"
+                );
+            ListConverter_ConvertToString_Test(
+                   new[] { '1', '2', '3', '4', '5' },
+                   "1,2,3,4,5",
+                   "ListConverter.ConvertToString should convert char list to string"
+                );
+            ListConverter_ConvertToString_Test(
+                   new[] { true, true, false, false },
+                   "True,True,False,False",
+                   "ListConverter.ConvertToString should convert bool list to string"
+                );
+            ListConverter_ConvertToString_Test(
+                   new[] { ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Cyan },
+                   "Black,Blue,Cyan",
+                   "ListConverter.ConvertToString should convert enum list to string"
+                );
+            ListConverter_ConvertToString_Test(
+                   new[] { new TimeSpan(1,0,0), new TimeSpan(0,0,30) },
+                   "01:00:00,00:00:30",
+                   "ListConverter.ConvertToString should convert TimeSpan to string"
+            );
+
+        }
+        #endregion ListConverter.ConvertToString tests
+
+        #region ListConverter.ConvertToList tests
+        public void ListConverter_ConvertToList_Test<T>(string list, IEnumerable<T> expected, string assertMessage) {
+            var actual = list.ConvertToList<T>();
+            Assert.IsTrue(expected.SequenceEqual(actual), assertMessage);
+        }
+
+        [TestMethod]
+        [TestCategory("ListConverter.ConvertToList")]
+        public void ListConverter_ConvertToList_Should_Convert_String_To_List() {
+            ListConverter_ConvertToList_Test(
+                   "1,2,3,4,5",
+                   new[] { 1, 2, 3, 4, 5 },
+                   "ListConverter.ConvertToList<int> should convert string to int list"
+                );
+            ListConverter_ConvertToList_Test(
+                   "1,2,3,4,5",
+                   new[] { '1', '2', '3', '4', '5' },
+                   "ListConverter.ConvertToList<char> should convert string to char list"
+                );
+            ListConverter_ConvertToList_Test(
+                   "True,True,False,False",
+                   new[] { true, true, false, false },
+                   "ListConverter.ConvertToList<bool> should convert string to bool list"
+                );
+            ListConverter_ConvertToList_Test(
+                   "Black,Blue,Cyan",
+                   new[] { ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Cyan },
+                   "ListConverter.ConvertToList<ConsoleColor> should convert string to enum list"
+                );
+            ListConverter_ConvertToList_Test(
+                   "1:00:00,0:00:30",
+                   new[] { new TimeSpan(1, 0, 0), new TimeSpan(0, 0, 30) },
+                   "ListConverter.ConvertToList<TimeSpan> should convert string to TimeSpan list"
+                );
+        }
+        #endregion ListConverter.ConvertToString tests
+
+        #region ArrayExtentions.SwapArrayElements tests
+        public void ArrayExtentions_SwapArrayElements_Test<T>(T[] data, int index1, int index2, T[] expected, string assertMessage) {
+            data.SwapArrayElements(index1, index2);
+            Assert.IsTrue(expected.SequenceEqual(data), assertMessage);
+        }
+
+        [TestMethod]
+        [TestCategory("ArrayExtentions.SwapArrayElements")]
+        public void ArrayExtentions_SwapArrayElements_Should_Swap_Array_Elemets() {
+            ArrayExtentions_SwapArrayElements_Test(
+                   new[] { 1, 2, 3, 4, 5 }, 0, 1,
+                   new[] { 2, 1, 3, 4, 5 },
+                   "ArrayExtentions.SwapArrayElements should swap the specified array elements"
+                );
+            ArrayExtentions_SwapArrayElements_Test(
+                   new[] { 1, 2, 3, 4, 5 }, 1, 0,
+                   new[] { 2, 1, 3, 4, 5 },
+                   "ArrayExtentions.SwapArrayElements should swap the specified array elements"
+                );
+            ArrayExtentions_SwapArrayElements_Test(
+                   new[] { "1", "2", "3", "4", "5" }, 1, 1,
+                   new[] { "1", "2", "3", "4", "5" },
+                   "ArrayExtentions.SwapArrayElements should not swap if index1==index2"
+                );
+        }
+
+        [TestMethod]
+        [TestCategory("ArrayExtentions.SwapArrayElements")]
+        [ExpectedException(typeof(IndexOutOfRangeException), 
+            "ArrayExtentions.SwapArrayElements should throw the IndexOutOfRangeException if index1 or index2 is out of range")]
+        public void ArrayExtentions_SwapArrayElements_Should_Throw_IndexOutOfRangeException_If_Indexes_Are_Out_Of_Range() {
+            ArrayExtentions_SwapArrayElements_Test(
+                   new[] { 1, 2, 3, 4, 5 }, 0, 10,
+                   new[] { 2, 1, 3, 4, 5 },
+                   string.Empty
+                );
+        }
+
+        #endregion ArrayExtentions.SwapArrayElements tests
+
+        #region ArrayExtentions.SortTupleArray tests
+        public void ArrayExtentions_SortTupleArray_Test(Tuple<int,int,int>[] data, int colIndex, bool desc, Tuple<int,int,int>[] expected, string assertMessage) {
+            data.SortTupleArray(colIndex, desc);
+            Assert.IsTrue(expected.SequenceEqual(data), assertMessage);
+        }
+
+        [TestMethod]
+        [TestCategory("ArrayExtentions.SortTupleArray")]
+        public void ArrayExtentions_SortTupleArray_Should_Sort_Array() {
+            ArrayExtentions_SortTupleArray_Test(
+                   new[] { Tuple.Create(3,8,8), Tuple.Create(2,9,9), Tuple.Create(1,10,10) }, 0, true,
+                   new[] { Tuple.Create(1,10,10), Tuple.Create(2,9,9), Tuple.Create(3,8,8) },
+                   "ArrayExtentions.SortTupleArray should sort array by sortedColumn=0 and ascending=true"
+                );
+            ArrayExtentions_SortTupleArray_Test(
+                   new[] { Tuple.Create(1, 12, 12), Tuple.Create(2, 11, 11), Tuple.Create(3, 10, 10) }, 0, false,
+                   new[] { Tuple.Create(3, 10, 10), Tuple.Create(2, 11, 11), Tuple.Create(1, 12, 12) },
+                   "ArrayExtentions.SortTupleArray should sort array by sortedColumn=0 and ascending=false"
+                );
+            ArrayExtentions_SortTupleArray_Test(
+                   new[] { Tuple.Create(8, 3, 8), Tuple.Create(9, 2, 9), Tuple.Create(10, 1, 10) }, 1, true,
+                   new[] { Tuple.Create(10, 1, 10), Tuple.Create(9, 2, 9), Tuple.Create(8, 3, 8) },
+                   "ArrayExtentions.SortTupleArray should sort array  by sortedColumn=1 and ascending=true"
+                );
+            ArrayExtentions_SortTupleArray_Test(
+                   new[] { Tuple.Create(12, 1, 12), Tuple.Create(11, 2, 11), Tuple.Create(10, 3, 10) }, 1, false,
+                   new[] { Tuple.Create(10, 3, 10), Tuple.Create(11, 2, 11), Tuple.Create(12, 1, 12) },
+                   "ArrayExtentions.SortTupleArray should sort array  by sortedColumn=1 and ascending=fasle"
+                );
+            ArrayExtentions_SortTupleArray_Test(
+                   new[] { Tuple.Create(8, 8, 3), Tuple.Create(9, 9, 2), Tuple.Create(10, 10, 1) }, 2, true,
+                   new[] { Tuple.Create(10, 10, 1), Tuple.Create(9, 9, 2), Tuple.Create(8, 8, 3) },
+                   "ArrayExtentions.SortTupleArray should sort array  by sortedColumn=2 and ascending=true"
+                );
+            ArrayExtentions_SortTupleArray_Test(
+                   new[] { Tuple.Create(12, 12, 1), Tuple.Create(11, 11, 2), Tuple.Create(10, 10, 3) }, 2, false,
+                   new[] { Tuple.Create(10, 10, 3), Tuple.Create(11, 11, 2), Tuple.Create(12, 12, 1) },
+                   "ArrayExtentions.SortTupleArray should sort array  by sortedColumn=2 and ascending=false"
+                );
+
+        }
+
+        [TestMethod]
+        [TestCategory("ArrayExtentions.SortTupleArray")]
+        [ExpectedException(typeof(IndexOutOfRangeException),
+            "ArrayExtentions.SortTupleArray should throw the IndexOutOfRangeException sortedColumn is out of range")]
+        public void ArrayExtentions_SortTupleArray_Should_Throw_IndexOutOfRangeException_If_sortedColumn_Is_Out_Of_Range() {
+            ArrayExtentions_SortTupleArray_Test(
+                   new[] { Tuple.Create(1, 1, 1) }, 5, true,
+                   new[] { Tuple.Create(1, 1, 1) },
+                   "ArrayExtentions.SortTupleArray should sort array  by sortedColumn=2 and descending=true"
+                );
+        }
+
+        #endregion ArrayExtentions.SortTupleArray tests
+
+        #region Singleton<T> tests
+
+        private const int SingletonIntValue = 100;
+        private const string SingletonStringValue = "Test";
+
+        public class MockSingleton {
+            public int IntProperty { get; set; }
+            public string StringProperty { get; set; }
+            public MockSingleton() {
+                IntProperty = SingletonIntValue;
+                StringProperty = SingletonStringValue;
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Singleton<T>")]
+        public void Singleton_Should_Create_Only_One_Instance() {
+            var instance1 = Singleton<MockSingleton>.Instance;
+            var instance2 = Singleton<MockSingleton>.Instance;
+            instance1.IntProperty++;
+            Assert.AreEqual(instance1.IntProperty, instance2.IntProperty, "Two instances of singleton reference to different objects");
+        }
+
+        [TestMethod]
+        [TestCategory("Singleton<T>")]
+        public void Singleton_Should_Be_Thread_Safe() {
+			int threadsCount = 10;
+			EventWaitHandle threadsFinished = new EventWaitHandle(false, EventResetMode.AutoReset);
+			MockSingleton[] singletons = new MockSingleton[threadsCount];
+			Thread[] threads = new Thread[threadsCount];
+			Barrier startBarrier = new Barrier(threadsCount);
+			Barrier finishBarrier = new Barrier(threadsCount, (barrier) => threadsFinished.Set());
+			for (int i = 0; i < threadsCount; i++) {
+				threads[i] = new Thread((idx) => {
+					startBarrier.SignalAndWait(); // Wait for creating all other threads
+					try {
+						singletons[(int)idx] = Singleton<MockSingleton>.Instance;
+					} catch { }
+					finishBarrier.SignalAndWait(); // Wait for finishing all other threads
+				});
+				threads[i].Start(i);
+			}
+			threadsFinished.WaitOne();
+			var inst = Singleton<MockSingleton>.Instance;
+			inst.IntProperty++;
+			Assert.IsTrue(singletons.All(x => x.IntProperty == inst.IntProperty), "Singleton<T> shoud inplement thread safe initialization");
+        }
+
+        #endregion Singleton<T> tests
+
+        #region FunctionExtention.TimeoutSafeInvoke tests
+        [TestMethod]
+        [TestCategory("FunctionExtention.TimeoutSafeInvoke")]
+        public void FunctionExtention_TimeoutSafeInvoke_Should_Invoke_Specified_Function() {
+            int magicInt = 1234;
+            Func<int> f = () => magicInt;
+            Assert.AreEqual(magicInt, f.TimeoutSafeInvoke(), "FunctionExtention.TimeoutSafeInvoke should invoke the specified function");
+        }
+
+        [TestMethod]
+        [TestCategory("FunctionExtention.TimeoutSafeInvoke")]
+        public void FunctionExtention_TimeoutSafeInvoke_Should_Catch_Up_To_2_Timeouts() {
+            StringWriter sw = new StringWriter();
+            TraceListener listener = new TextWriterTraceListener(sw);
+            Trace.Listeners.Add(listener);
+            int magicInt = 1234;
+            int tries = 0;
+            Func<int> f = () => {
+				if (tries++ < 2)
+					throw new WebException("The operation has timed out", WebExceptionStatus.Timeout);
+				else
+					return magicInt;
+            };
+            Assert.AreEqual(magicInt, f.TimeoutSafeInvoke(), "FunctionExtention.TimeoutSafeInvoke should invoke the specified function");
+            Trace.Listeners.Remove(listener);
+            listener.Flush();
+            sw.Close();
+            string trace = sw.ToString();
+            Assert.IsTrue(trace.Contains("System.Net.WebException"), "FunctionExtention.TimeoutSafeInvoke should log a WebException to trace log");
+        }
+
+        [TestMethod]
+        [TestCategory("FunctionExtention.TimeoutSafeInvoke")]
+        [ExpectedException(typeof(WebException))]
+        public void FunctionExtention_TimeoutSafeInvoke_Should_Raise_TimeoutException_If_Number_Of_Timeouts_More_Then_2() {
+            int magicInt = 1234;
+            int tries = 0;
+            Func<int> f = () => {
+				if (tries++ < 3)
+					throw new WebException("The operation has timed out", WebExceptionStatus.Timeout);
+				else
+					return magicInt;
+            };
+            f.TimeoutSafeInvoke();
+        }
+
+        #endregion FunctionExtention.TimeoutSafeInvoke tests
+
+        #region FunctionExtention.CombinePredicates tests
+        [TestMethod]
+        [TestCategory("FunctionExtention.CombinePredicates")]
+        public void FunctionExtention_CombinePredicates_Should_Combine_String_Predicates() {
+            var result = FunctionExtentions.CombinePredicates(new Predicate<string>[] {
+                    x=> !string.IsNullOrEmpty(x),
+                    x=> x.StartsWith("START"),
+                    x=> x.EndsWith("END"),
+                    x=> x.Contains("#")
+                });
+            Assert.IsTrue(result("START # END"));
+            Assert.IsTrue(result("START######END"));
+            Assert.IsTrue(result("START 1111#1111 END"));
+            Assert.IsFalse(result("START END"));
+            Assert.IsFalse(result(string.Empty));
+            Assert.IsFalse(result("START #"));
+        }
+
+        [TestMethod]
+        [TestCategory("FunctionExtention.CombinePredicates")]
+        public void FunctionExtention_CombinePredicates_Should_Combine_Int_Predicates() {
+            var result = FunctionExtentions.CombinePredicates(new Predicate<int>[] {
+                    x=> x>-10,
+                    x=> x<10,
+                    x=> x!=0,
+                    x=> x!=1
+                });
+            Assert.IsTrue(result(2));
+            Assert.IsTrue(result(-5));
+            Assert.IsTrue(result(9));
+            Assert.IsFalse(result(-20));
+            Assert.IsFalse(result(0));
+            Assert.IsFalse(result(1));
+        }
+
+        [TestMethod]
+        [TestCategory("FunctionExtention.CombinePredicates")]
+        public void FunctionExtention_CombinePredicates_Should_Combine_Single_Predicates() {
+            var result = FunctionExtentions.CombinePredicates(new Predicate<int>[] {
+                    x=> x>0,
+                });
+            Assert.IsTrue(result(2));
+            Assert.IsTrue(result(5));
+            Assert.IsTrue(result(1000));
+            Assert.IsFalse(result(-20));
+            Assert.IsFalse(result(0));
+            Assert.IsFalse(result(-1000));
+        }
+
+        #endregion FunctionExtention.CombinePredicates tests
+
+    }
+
+}
+
+
---
+++ 02-Generics/Generics/Generics.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+
+namespace Task.Generics
+{
+
+    public static class ListConverter
+    {
+
+        private static char ListSeparator = ',';  // Separator used to separate values in string
+
+        /// <summary>
+        ///   Converts a source list into a string representation
+        /// </summary>
+        /// <typeparam name="T">type  of list items</typeparam>
+        /// <param name="list">source list</param>
+        /// <returns>
+        ///   Returns the string representation of a list 
+        /// </returns>
+        /// <example>
+        ///   { 1,2,3,4,5 } => "1,2,3,4,5"
+        ///   { '1','2','3','4','5'} => "1,2,3,4,5"
+        ///   { true, false } => "True,False"
+        ///   { ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Cyan } => "Black,Blue,Cyan"
+        ///   { new TimeSpan(1, 0, 0), new TimeSpan(0, 0, 30) } => "01:00:00,00:00:30",
+        /// </example>
+        public static string ConvertToString<T>(this IEnumerable<T> list)
+        {
+            return string.Join(ListSeparator.ToString(), list);
+        }
+
+        /// <summary>
+        ///   Converts the string respresentation to the list of items
+        /// </summary>
+        /// <typeparam name="T">required type of output items</typeparam>
+        /// <param name="list">string representation of the list</param>
+        /// <returns>
+        ///   Returns the list of items from specified string
+        /// </returns>
+        /// <example>
+        ///  "1,2,3,4,5" for int => {1,2,3,4,5}
+        ///  "1,2,3,4,5" for char => {'1','2','3','4','5'}
+        ///  "1,2,3,4,5" for string => {"1","2","3","4","5"}
+        ///  "true,false" for bool => { true, false }
+        ///  "Black,Blue,Cyan" for ConsoleColor => { ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Cyan }
+        ///  "1:00:00,0:00:30" for TimeSpan =>  { new TimeSpan(1, 0, 0), new TimeSpan(0, 0, 30) },
+        ///  </example>
+        public static IEnumerable<T> ConvertToList<T>(this string list)
+        {
+            TypeConverter typeConverter = TypeDescriptor.GetConverter(typeof(T));
+            foreach (var a in list.Split(ListSeparator))
+            {
+                yield return (T)typeConverter.ConvertFromString(a);
+            }
+        }
+
+    }
+
+    public static class ArrayExtentions
+    {
+
+        /// <summary>
+        ///   Swaps the one element of source array with another
+        /// </summary>
+        /// <typeparam name="T">required type of</typeparam>
+        /// <param name="array">source array</param>
+        /// <param name="index1">first index</param>
+        /// <param name="index2">second index</param>
+        public static void SwapArrayElements<T>(this T[] array, int index1, int index2)
+        {
+            if (index1 != index2)
+            {
+                var temp = array[index1];
+                array[index1] = array[index2];
+                array[index2] = temp;
+            }
+        }
+
+        /// <summary>
+        ///   Sorts the tuple array by specified column in ascending or descending order
+        /// </summary>
+        /// <param name="array">source array</param>
+        /// <param name="sortedColumn">index of column</param>
+        /// <param name="ascending">true if ascending order required; otherwise false</param>
+        /// <example>
+        ///   source array : 
+        ///   { 
+        ///     { 1, "a", false },
+        ///     { 3, "b", false },
+        ///     { 2, "c", true  }
+        ///   }
+        ///   result of SortTupleArray(array, 0, true) is sort rows by first column in a ascending order: 
+        ///   { 
+        ///     { 1, "a", false },
+        ///     { 2, "c", true  },
+        ///     { 3, "b", false }
+        ///   }
+        ///   result of SortTupleArray(array, 1, false) is sort rows by second column in a descending order: 
+        ///   {
+        ///     { 2, "c", true  },
+        ///     { 3, "b", false }
+        ///     { 1, "a", false },
+        ///   }
+        /// </example>
+        public static void SortTupleArray<T1, T2, T3>(this Tuple<T1, T2, T3>[] array, int sortedColumn, bool ascending)
+            where T1 : IComparable
+            where T2 : IComparable
+            where T3 : IComparable
+        {
+            int comp = ascending ? 1 : -1;
+            switch (sortedColumn)
+            {
+                case 0: { Array.Sort(array, (element1, element2) => element1.Item1.CompareTo(element2.Item1) * comp); break; }
+                case 1: { Array.Sort(array, (element1, element2) => element1.Item2.CompareTo(element2.Item2) * comp); break; }
+                case 2: { Array.Sort(array, (element1, element2) => element1.Item3.CompareTo(element2.Item3) * comp); break; }
+                default: { throw new IndexOutOfRangeException(); }
+            }
+
+        }
+    }
+
+    /// <summary>
+    ///   Generic singleton class
+    /// </summary>
+    /// <example>
+    ///   This code should return the same MyService object every time:
+    ///   MyService singleton = Singleton<MyService>.Instance;
+    /// </example>
+    public static class Singleton<T>
+            where T : new()
+    {
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new T();
+                return instance;
+            }
+        }
+    }
+
+
+
+    public static class FunctionExtentions
+    {
+        /// <summary>
+        ///   Tries to invoke the specified function up to 3 times if the result is unavailable 
+        /// </summary>
+        /// <param name="function">specified function</param>
+        /// <returns>
+        ///   Returns the result of specified function, if WebException occurs duaring request then exception should be logged into trace 
+        ///   and the new request should be started (up to 3 times).
+        /// </returns>
+        /// <example>
+        ///   Sometimes if network is unstable it is required to try several request to get data:
+        ///   
+        ///   Func<string> f1 = ()=>(new System.Net.WebClient()).DownloadString("http://www.google.com/");
+        ///   string data = f1.TimeoutSafeInvoke();
+        ///   
+        ///   If the first attemp to download data is failed by WebException then exception should be logged to trace log and the second attemp should be started.
+        ///   The second attemp has the same workflow.
+        ///   If the third attemp fails then this exception should be rethrow to the application.
+        /// </example>
+        public static T TimeoutSafeInvoke<T>(this Func<T> function)
+        {
+            int counter = 0;
+            bool isError = false;
+            do
+            {
+                try
+                {
+                    return function();
+                }
+                catch (WebException webException)
+                {
+                    isError = true;
+                    counter++;
+                    Trace.WriteLine(webException);
+                }
+            }
+            while (isError && counter < 3);
+            throw new WebException();
+        }
+
+
+        /// <summary>
+        ///   Combines several predicates using logical AND operator 
+        /// </summary>
+        /// <param name="predicates">array of predicates</param>
+        /// <returns>
+        ///   Returns a new predicate that combine the specified predicated using AND operator
+        /// </returns>
+        /// <example>
+        ///   var result = CombinePredicates(new Predicate<string>[] {
+        ///            x=> !string.IsNullOrEmpty(x),
+        ///            x=> x.StartsWith("START"),
+        ///            x=> x.EndsWith("END"),
+        ///            x=> x.Contains("#")
+        ///        })
+        ///   should return the predicate that identical to 
+        ///   x=> (!string.IsNullOrEmpty(x)) && x.StartsWith("START") && x.EndsWith("END") && x.Contains("#")
+        ///
+        ///   The following example should create predicate that returns true if int value between -10 and 10:
+        ///   var result = CombinePredicates(new Predicate<int>[] {
+        ///            x=> x>-10,
+        ///            x=> x<10
+        ///       })
+        /// </example>
+        public static Predicate<T> CombinePredicates<T>(Predicate<T>[] predicates)
+        {
+            Predicate<T> pred = delegate (T x)
+            {
+                foreach (var a in predicates)
+                {
+                    if (a(x) == false)
+                        return false;
+                }
+                return true;
+            };
+            return pred;
+        }
+
+    }
+}
