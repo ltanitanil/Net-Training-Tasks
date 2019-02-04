fbb57b2412099e6d6f51a203a2bac6dda32742e8
tanu2 <tanu2@DESKTOP-I8E9H25>
2019-02-04 09:45:21 +0300

    added task7

--- 07-IO Streams/IOStreams/TestTasks.cs
+++ 07-IO Streams/IOStreams/TestTasks.cs
@@ -12,20 +12,20 @@ using System.Xml.Linq;
 namespace IOStreams
 {
 
-	public static class TestTasks
-	{
-		/// <summary>
-		/// Parses Resourses\Planets.xlsx file and returns the planet data: 
-		///   Jupiter     69911.00
-		///   Saturn      58232.00
-		///   Uranus      25362.00
-		///    ...
-		/// See Resourses\Planets.xlsx for details
-		/// </summary>
-		/// <param name="xlsxFileName">source file name</param>
-		/// <returns>sequence of PlanetInfo</returns>
-		public static IEnumerable<PlanetInfo> ReadPlanetInfoFromXlsx(string xlsxFileName)
-		{
+    public static class TestTasks
+    {
+        /// <summary>
+        /// Parses Resourses\Planets.xlsx file and returns the planet data: 
+        ///   Jupiter     69911.00
+        ///   Saturn      58232.00
+        ///   Uranus      25362.00
+        ///    ...
+        /// See Resourses\Planets.xlsx for details
+        /// </summary>
+        /// <param name="xlsxFileName">source file name</param>
+        /// <returns>sequence of PlanetInfo</returns>
+        public static IEnumerable<PlanetInfo> ReadPlanetInfoFromXlsx(string xlsxFileName)
+        {
             XNamespace xNamespace = "http://schemas.openxmlformats.org/spreadsheetml/2006/main";
 
             //  /xl/sharedStrings.xml      - dictionary of all string values
@@ -41,8 +41,8 @@ namespace IOStreams
         public static XElement ReadPlanetInfoFromXlsx1(string xlsxFileName, string uri)
         {
             using (Package package = Package.Open(xlsxFileName, FileMode.Open, FileAccess.Read))
-                using (Stream stream = package.GetPart(new Uri(uri, UriKind.Relative)).GetStream())
-                    return XElement.Load(stream);
+            using (Stream stream = package.GetPart(new Uri(uri, UriKind.Relative)).GetStream())
+                return XElement.Load(stream);
         }
 
 
@@ -53,7 +53,7 @@ namespace IOStreams
         /// <param name="hashAlgorithmName">hash algorithm ("MD5","SHA1","SHA256" and other supported by .NET)</param>
         /// <returns></returns>
         public static string CalculateHash(this Stream stream, string hashAlgorithmName)
-		{
+        {
             var algorithm = HashAlgorithm.Create(hashAlgorithmName);
             if (algorithm == null)
                 throw new ArgumentException();
@@ -61,14 +61,14 @@ namespace IOStreams
         }
 
 
-		/// <summary>
-		/// Returns decompressed strem from file. 
-		/// </summary>
-		/// <param name="fileName">source file</param>
-		/// <param name="method">method used for compression (none, deflate, gzip)</param>
-		/// <returns>output stream</returns>
-		public static Stream DecompressStream(string fileName, DecompressionMethods method)
-		{
+        /// <summary>
+        /// Returns decompressed strem from file. 
+        /// </summary>
+        /// <param name="fileName">source file</param>
+        /// <param name="method">method used for compression (none, deflate, gzip)</param>
+        /// <returns>output stream</returns>
+        public static Stream DecompressStream(string fileName, DecompressionMethods method)
+        {
             Stream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
             if (method == DecompressionMethods.GZip)
                 return new GZipStream(stream, 0);
@@ -78,36 +78,36 @@ namespace IOStreams
         }
 
 
-		/// <summary>
-		/// Reads file content econded with non Unicode encoding
-		/// </summary>
-		/// <param name="fileName">source file name</param>
-		/// <param name="encoding">encoding name</param>
-		/// <returns>Unicoded file content</returns>
-		public static string ReadEncodedText(string fileName, string encoding)
-		{
+        /// <summary>
+        /// Reads file content econded with non Unicode encoding
+        /// </summary>
+        /// <param name="fileName">source file name</param>
+        /// <param name="encoding">encoding name</param>
+        /// <returns>Unicoded file content</returns>
+        public static string ReadEncodedText(string fileName, string encoding)
+        {
             using (var file = new StreamReader(fileName, Encoding.GetEncoding(encoding)))
                 return file.ReadToEnd();
         }
-	}
+    }
 
 
-	public class PlanetInfo : IEquatable<PlanetInfo>
-	{
-		public string Name { get; set; }
-		public double MeanRadius { get; set; }
+    public class PlanetInfo : IEquatable<PlanetInfo>
+    {
+        public string Name { get; set; }
+        public double MeanRadius { get; set; }
 
-		public override string ToString()
-		{
-			return string.Format("{0} {1}", Name, MeanRadius);
-		}
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Name, MeanRadius);
+        }
 
-		public bool Equals(PlanetInfo other)
-		{
-			return Name.Equals(other.Name)
-				&& MeanRadius.Equals(other.MeanRadius);
-		}
-	}
+        public bool Equals(PlanetInfo other)
+        {
+            return Name.Equals(other.Name)
+                && MeanRadius.Equals(other.MeanRadius);
+        }
+    }
 
 
 
